1efbf7a3518859a3eb33ee7b95a8060a0a467115
deerain <d33.i2.1n@gmail.com>
2023-06-22 20:54:38 +0300

    Удалил главное приложение

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,13 @@ from django.views import generic
 from blog import services as blog_services
 
 
+class PostList(generic.ListView):
+    template_name = 'main/home.html'
+    model = blog_services.models.Post
+
+    def get_queryset(self):
+        return blog_services.post_all().filter(visible=True)
+
+
 class PostDetail(generic.DetailView):
     model = blog_services.models.Post
--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 
     'ckeditor',
     'blog',
-    'main',
     'portfolio',
 ]
 
